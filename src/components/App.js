59cb1cac927b8118a66839b0eaf26a76c3f2d033
AlexKriv <krivouzis1985@gmail.com>
2023-04-24 14:11:41 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+
+export const App = () => {
+  return (
+    <Layout>
+      <div>Recipe Form</div>
+      <div>Image modal</div>
+      <GlobalStyle />
+    </Layout>
+  );
+};
